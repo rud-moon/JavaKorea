05048975e892d4f31f1f1419b99c5888f1f8b2c6
rud.moon <cynthia210@naver.com>
2022-07-01 23:23:13 +0900

    java 1일차

---
+++ myweb/src/main/java/com/example/myweb/MywebApplication.java
@@ -0,0 +1,14 @@
+package com.example.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebApplication.class, args);
+		System.out.println("실행 완료");
+	}
+
+}
