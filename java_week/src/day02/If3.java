8f7c19d3c052b9e1c82e3bf110f917e5816dfae9
rud.moon <cynthia210@naver.com>
2022-07-02 00:26:01 +0900

    java2일

--- java_week/src/day02/If3.java
+++ java_week/src/day02/If3.java
@@ -9,14 +9,14 @@ public class If3 {
 //			System.out.println("미성년자");
 //		}
 //		
-		int 성적 = 87;
+		int 성적 = 79;
 		if(성적 >= 90) {
 			System.out.println("A");
-		}if(성적<90 && 성적>=80) {
+		}else if(성적>=80) {
 			System.out.println("B");
-		}if(성적<80 && 성적>=70) {
+		}else if(성적>=70) {
 			System.out.println("C");
-		}if(성적 <70) {
+		}else{
 			System.out.println("F");
 		}
 	}
--- myweb/src/main/java/com/example/myweb/MyController.java
+++ myweb/src/main/java/com/example/myweb/MyController.java
@@ -1,12 +1,30 @@
 package com.example.myweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
+	
 	@GetMapping("/")			// 주소창에 localhost:8081 / 입력하면
 	public String indexPage() {
 		return "index";			// index.html 파일을 열어라
+	}
+	
+	@GetMapping("oper")		//localhost:8081/oper
+	public String oper(Model model, @RequestParam("num1") String num) {
+		String msg = "숫자를 입력하세요";
+		int 나이 = Integer.parseInt(num);
+		if(나이 >= 20) {
+			msg = "성인 입니다.";
+		}else if(나이 >= 14) {
+			msg = "미성년자 입니다.";
+		}else {
+			msg="어린이입니다.";
+		}
+		model.addAttribute("result",msg); //result 라는 키워드로 화면에 보내기
+		return "oper";				//oper.html 열어라
 	}
 }
